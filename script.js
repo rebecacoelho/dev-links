2ec9fb89ca25cd90878a764416fc90d98efdb851
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2023-01-07 14:14:34 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement // documentElemnt = elemento HTML
+  html.classList.toggle('light');
+
+  /*
+  if(html.classList.contains('light')) {
+    html.classList.remove('light');
+  } else {
+    html.classList.add('light');
+  }
+  */
+
+  // pegar a tag img 
+  const img = document.querySelector(".profile img")
+  // substituir a imagem
+  if(html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png ')
+  }
+}
